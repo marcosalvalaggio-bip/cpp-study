b0e82a801d5beca15874fe757e11dc8489e6b3b3
marcosalvalaggio_bip <marco.salvalaggio@mail-bip.com>
2023-07-01 13:32:39 +0200

    wip

--- lab.cpp
+++ lab.cpp
@@ -4,18 +4,34 @@
 #include <algorithm>
 #include <string>
 
+// CLASS 
+// lecture: https://www.youtube.com/watch?v=2BP8NhxjrO0&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=18
+// everything inside a class is private (public in struct)
 
 #define LOG(x) std::cout << x << std::endl;
 
+
+class Person {
+    public:
+        int x, y, speed;
+
+        void Move(int xa, int ya) {
+            x = xa * speed;
+            y = ya * speed;
+        }
+};
+
 int main() {
 
-    char letter = 'a';
-    std::string stringa = "stringa";
-    LOG(letter); 
-    std::cout << sizeof(letter) << std::endl;
-    LOG(stringa);
-    std::cout << sizeof(stringa) << std::endl;
-    std::cout << stringa.size() << std::endl;
+    Person person;
+    person.x = 0;
+    person.y = 0;
+    LOG(person.x);
+    LOG(person.y);
+    person.speed = 1;
+    person.Move(3,3);
+    LOG(person.x);
+    LOG(person.y);
 
     return 0;
 }
